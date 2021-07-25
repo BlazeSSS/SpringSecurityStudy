759b2dc940ebd7f3d63457bb199a9be9ff30cc44
BlazeSSS <1136296108@qq.com>
2021-07-26 01:13:07 +0800

    feature: 自定义登录用户 / 连接 MySQL

--- src/main/java/com/example/springsecurity/config/SecurityConfig4.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig4.java
@@ -1,5 +1,6 @@
 package com.example.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -10,21 +11,32 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-//@Configuration
-public class SecurityConfig3 extends WebSecurityConfigurerAdapter {
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig4 extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("admin").password("123").roles("admin").build());
-        manager.createUser(User.withUsername("avalon").password("123").roles("user").build());
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setDataSource(dataSource);
+        if (!manager.userExists("admin")) {
+            manager.createUser(User.withUsername("admin").password("123").roles("admin").build());
+        }
+        if (!manager.userExists("avalon")) {
+            manager.createUser(User.withUsername("avalon").password("123").roles("user").build());
+        }
         return manager;
     }
 
@@ -37,13 +49,16 @@ public class SecurityConfig3 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 配置权限的拦截规则
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("admin")
                 .antMatchers("/user/**").hasRole("user")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .defaultSuccessUrl("/index");
+                .defaultSuccessUrl("/index")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/byebye")
+                .permitAll();
     }
 }
